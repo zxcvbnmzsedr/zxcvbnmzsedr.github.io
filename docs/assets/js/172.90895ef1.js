b6de7c2c10d96c7a5f5d6731490c23b06a56732a
zhaotianzeng <tianzneg@gmail.com>
2022-08-22 15:51:38 +0800

    Updates

--- docs/assets/js/172.90895ef1.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[172],{476:function(t,s,n){"use strict";n.r(s);var e=n(6),a=Object(e.a)({},(function(){var t=this._self._c;return t("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[t("p",[this._v("Seata 会有 4 种分布式事务解决⽅案，分别是 AT 模式、TCC 模式、Saga模式和 XA 模式。")]),this._v(" "),t("p")])}),[],!1,null,null,null);s.default=a.exports}}]);
